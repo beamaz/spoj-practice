3552c717acdbeaa989d1bebf0d0997442ff1543d
beamaz <mazgaj.beata@gmail.com>
2021-01-24 11:51:55 +0100

    added-kabbalah-unit-tests

---
+++ KabalistycznyZapisDaty/KabbalahConverter.cs
@@ -0,0 +1,58 @@
+﻿namespace KabalistycznyZapisDaty
+{
+    public static class KabbalahConverter
+    {
+        public static int ConvertCharToKabbalahNumber(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    return 1;
+                case 'B':
+                    return 2;
+                case 'C':
+                    return 3;
+                case 'D':
+                    return 4;
+                case 'E':
+                    return 5;
+                case 'F':
+                    return 6;
+                case 'G':
+                    return 7;
+                case 'H':
+                    return 8;
+                case 'I':
+                    return 9;
+                case 'K':
+                    return 10;
+                case 'L':
+                    return 20;
+                case 'M':
+                    return 30;
+                case 'N':
+                    return 40;
+                case 'O':
+                    return 50;
+                case 'P':
+                    return 60;
+                case 'Q':
+                    return 70;
+                case 'R':
+                    return 80;
+                case 'S':
+                    return 90;
+                case 'T':
+                    return 100;
+                case 'V':
+                    return 200;
+                case 'X':
+                    return 300;
+                case 'Y':
+                    return 400;
+                default:
+                    return 500;
+            }
+        }
+    }
+}
---
+++ UnitTests/KabalistycznyZapisDaty/KabbalahConverterTest.cs
@@ -0,0 +1,23 @@
+﻿using KabalistycznyZapisDaty;
+using NUnit.Framework;
+
+namespace UnitTests.KabalistycznyZapisDaty
+{
+    [TestFixture]
+    public class KabbalahConverterTest
+    {
+        [Test]
+        [TestCase('A', ExpectedResult = 1)]
+        [TestCase('H', ExpectedResult = 8)]
+        [TestCase('O', ExpectedResult = 50)]
+        [TestCase('T', ExpectedResult = 100)]
+        [TestCase('V', ExpectedResult = 200)]
+        [TestCase('X', ExpectedResult = 300)]
+        [TestCase('Y', ExpectedResult = 400)]
+
+        public int ShouldCorrectlyReturnTheNumber(char input)
+        {
+            return KabbalahConverter.ConvertCharToKabbalahNumber(input);
+        }
+    }
+}
